2114f3dd7465383d1c1d17358d011f284992197c
mishrasunny-coder <mishra.sunny@gmail.com>
2025-02-11 22:07:22 -0500

    setting up db

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from projects.models import Project
 # from django.http import HttpResponse
 
 # Create your views here.
-def project_list(request):
+# def project_list(request):
     # return HttpResponse("<h1>Hello Everyone!</h1>")
-    return render(request, 'projects/index.html')
+    # return render(request, 'projects/index.html')
+
+def all_projects(request):
+    # query database
+    projects = Project.objects.all()
+    # print(projects)
+    return render(request, 'projects/all_projects.html', {'projects': projects})
