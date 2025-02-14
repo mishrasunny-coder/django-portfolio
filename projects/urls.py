24904e682ecf0464dd95eeeac30fb8999bff54c7
mishrasunny-coder <mishra.sunny@gmail.com>
2025-02-13 20:56:49 -0500

    display the project

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', views.all_projects),
+    path('', views.all_projects, name='all_projects'),
     # path('test', views.project_list),
+    path('<int:pk>', views.project_detail, name='project_detail'),
 
 ]
